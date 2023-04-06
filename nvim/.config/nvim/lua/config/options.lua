a68293081651f578ee2dcf5b742c8f4a4fb2df8c
kyriekevin <2718629413@qq.com>
2023-04-06 21:40:28 +0800

    add neovim's vimwiki plugin

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -1,24 +1,21 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
-local opt = vim.opt  -- for conciseness
+local opt = vim.opt -- for conciseness
 
 local options = {
-  backup = false,                         -- creates a backup file
-  swapfile = false,                       -- creates a swapfile
-  wildmenu = true,                        -- show a navigable menu for completion
+  backup = false, -- creates a backup file
+  swapfile = false, -- creates a swapfile
+  wildmenu = true, -- show a navigable menu for completion
   autoindent = true,
-  listchars = {tab = '| ', trail = '.'},
+  listchars = { tab = "| ", trail = "." },
 
   ttyfast = true,
-  lazyredraw = true,
   visualbell = true,
 
-  backspace = "indent,eol,start"
+  backspace = "indent,eol,start",
 }
 
 for k, v in pairs(options) do
   opt[k] = v
 end
-
-
