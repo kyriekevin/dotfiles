0a206811e3d3d44aac9ea552b446f723f136b450
kyriekevin <2718629413@qq.com>
2023-07-22 10:10:56 +0800

    update brew list and zsh config

--- brewinstall.sh
+++ brewinstall.sh
@@ -1,2 +1,2 @@
-brew install fzf git wget ranger tmux neovim node neofetch stow;
-brew install --cask --no-quarantine google-chrome iterm2 qq sogouinput wechat visual-studio-code mos miniconda
+brew install fzf git wget ranger tmux neovim node neofetch stow
+brew install --cask --no-quarantine google-chrome iterm2 qq sogouinput wechat visual-studio-code mos miniconda neovide
--- zsh/.zshrc
+++ zsh/.zshrc
@@ -79,6 +79,8 @@ ZSH_THEME="powerlevel10k/powerlevel10k"
 # Example format: plugins=(rails git textmate ruby lighthouse)
 # Add wisely, as too many plugins slow down shell startup.
 plugins=(git
+  z
+  brew
 	extract
 	zsh-autosuggestions
 	zsh-syntax-highlighting
