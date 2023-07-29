1364483d50316cd0ae53fe1c65bfebfbb4ba665c
钟宇哲 <zhongyuzhe@192.168.0.103>
2023-07-29 22:50:19 +0800

    add sketchybar config

---
+++ sketchybar.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# install sketchybar
+brew tap FelixKratz/formulae
+brew install sketchybar
+
+# install dependencies
+brew install --cask sf-symbols
+brew install jq
+brew install switchaudio-osx
+curl -L https://github.com/kvndrsslr/sketchybar-app-font/releases/download/v1.0.4/sketchybar-app-font.ttf -o $HOME/Library/Fonts/sketchybar-app-font.ttf
+
+# start sketchybar
+brew services start sketchybar
