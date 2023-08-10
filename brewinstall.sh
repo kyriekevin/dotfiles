f49c2c608b1777cd8bd6253215e090089e8b71f2
钟宇哲 <zhongyuzhe@192.168.0.102>
2023-08-10 20:48:37 +0800

    update list of brew to install

--- brewinstall.sh
+++ brewinstall.sh
@@ -1,2 +1,2 @@
 brew install fzf git wget ranger tmux neovim node neofetch stow lazygit ripgrep fd python@3.9 koekeishiya/formulae/yabai joshuto
-brew install --cask --no-quarantine google-chrome iterm2 qq sogouinput wechat visual-studio-code mos miniconda neovide raycast
+brew install --cask --no-quarantine google-chrome iterm2 qq sogouinput wechat visual-studio-code mos miniconda neovide spotify raycast anki
