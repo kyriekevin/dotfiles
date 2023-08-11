3928284253eeceb8fd4065f800f365d68d3b2a66
kyriekevin <2718629413@qq.com>
2023-08-11 13:12:07 +0800

    add eslient and ues it for formatting

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -4,4 +4,18 @@ return {
     local keys = require("lazyvim.plugins.lsp.keymaps").get()
     keys[#keys + 1] = { "K", false }
   end,
+  opts = {
+    servers = { eslint = {} },
+    setup = {
+      eslint = function()
+        require("lazyvim.util").on_attach(function(client)
+          if client.name == "eslint" then
+            client.server_capabilities.documentFormattingProvider = true
+          elseif client.name == "tsserver" then
+            client.server_capabilities.documentFormattingProvider = false
+          end
+        end)
+      end,
+    },
+  },
 }
