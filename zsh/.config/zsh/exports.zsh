c14524769537d93f82c1148ea134a9dbe25cf459
kyriekevin <2718629413@qq.com>
2023-08-27 15:10:15 +0800

    add switch vpn func to manage self and company vpn

--- zsh/.config/zsh/exports.zsh
+++ zsh/.config/zsh/exports.zsh
@@ -8,6 +8,28 @@ export HOMEBREW_BREW_GIT_REMOTE="https://mirrors.tuna.tsinghua.edu.cn/git/homebr
 export HOMEBREW_CORE_GIT_REMOTE="https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/homebrew-core.git"
 export HOMEBREW_PIP_INDEX_URL="https://pypi.tuna.tsinghua.edu.cn/simple"
 
-export https_proxy=http://127.0.0.1:7890 http_proxy=http://127.0.0.1:7890 all_proxy=socks5://127.0.0.1:7890
-
 export EDITOR=nvim
+
+# vpn
+CURRENT_VPN="default"
+function sv() {
+    if [[ $CURRENT_VPN == "company" ]]; then
+        export http_proxy="http://127.0.0.1:7890"
+        export https_proxy="http://127.0.0.1:7890"
+        export all_proxy="socks5://127.0.0.1:7890"
+        unset HTTP_PROXY
+        unset HTTPS_PROXY
+        CURRENT_VPN="default"
+        echo "Switched to default VPN settings"
+    else
+        export http_proxy="http://proxy.sensetime.com:3128/"
+        export https_proxy="http://proxy.sensetime.com:3128/"
+        export HTTP_PROXY="http://proxy.sensetime.com:3128/"
+        export HTTPS_PROXY="http://proxy.sensetime.com:3128/"
+        unset all_proxy
+        CURRENT_VPN="company"
+        echo "Switched to company VPN settings"
+    fi
+}
+
+
