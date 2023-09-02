d19ac9416d05393906bb8e1119f344c32299a57a
kyriekevin <2718629413@qq.com>
2023-09-03 01:52:32 +0800

    feat(nvim): Add initial configuration files for neovim

---
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -0,0 +1,23 @@
+local map = vim.keymap.set
+
+-- Key mappings --
+map({ "n", "i", "v" }, "<Left>", "<Nop>")
+map({ "n", "i", "v" }, "<Right>", "<Nop>")
+map({ "n", "i", "v" }, "<Up>", "<Nop>")
+map({ "n", "i", "v" }, "<Down>", "<Nop>")
+
+-- save & quit
+map({ "i", "x", "n", "s" }, "<C-s>", "<cmd>w<cr><esc>", { desc = "Save file" })
+map({ "i", "x", "n", "s" }, "<C-q>", "<cmd>q<cr><esc>", { desc = "Quit" })
+
+-- fast move
+map({ "n", "v" }, "<S-j>", "5j")
+map({ "n", "v" }, "<S-k>", "5k")
+map({ "n", "v" }, "<S-w>", "5w")
+map({ "n", "v" }, "<S-b>", "5b")
+map({ "n", "v" }, "<S-h>", "0")
+map({ "n", "v" }, "<S-l>", "$")
+
+-- better indenting
+map({ "v" }, "<", "<gv")
+map({ "v" }, ">", ">gv")
---
+++ nvim/.config/nvim/lua/config/options.lua
@@ -0,0 +1,55 @@
+local opt = vim.opt
+
+-- Buffer Settings --
+vim.b.fileencoding = "utf-8"
+
+-- Global Settings --
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+-- Globol Settings --
+opt.autowrite = true -- Enable auto write
+opt.clipboard = "unnamedplus" -- Sync with system clipboard
+opt.completeopt = "menu,menuone,noselect"
+opt.conceallevel = 3 -- Hide * markup for bold and italic
+opt.confirm = true -- Confirm to save changes before exiting modified buffer
+opt.showmode = false
+opt.wildmenu = true
+opt.backspace = { "indent", "eol", "start" }
+opt.list = true
+opt.spelllang = { "en" }
+opt.splitbelow = true -- Put new windows below current
+opt.splitright = true -- Put new windows right of current
+
+-- tab
+opt.tabstop = 2
+opt.shiftwidth = 2
+opt.expandtab = true
+opt.shiftround = true
+opt.autoindent = true
+opt.smartindent = true
+
+-- number
+opt.number = true
+opt.relativenumber = true
+
+-- search
+opt.hlsearch = false
+opt.ignorecase = true
+opt.smartcase = true
+
+opt.cursorline = true
+opt.termguicolors = true
+opt.signcolumn = "yes"
+opt.autoread = true
+opt.title = true
+opt.swapfile = false
+opt.backup = false
+opt.updatetime = 50
+opt.mouse = ""
+opt.undofile = true
+opt.undodir = vim.fn.expand('$HOME/.local/share/nvim/undo')
+opt.exrc = true
+opt.wrap = false
+opt.wildmode = "longest:full,full" -- Command-line completion mode
+
