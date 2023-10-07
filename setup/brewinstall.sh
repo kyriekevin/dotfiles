914dc5bd69f586436e04d18c1909ba3725f6dbe0
kyriekevin <2718629413@qq.com>
2023-10-08 00:06:10 +0800

    [setup/brewinstall.sh] Add trash to brew install list, for better file deletion handling

--- setup/brewinstall.sh
+++ setup/brewinstall.sh
@@ -1,2 +1,2 @@
-brew install fzf git wget ranger tmux neovim node neofetch stow lazygit ripgrep fd python@3.9 koekeishiya/formulae/yabai joshuto exa bat bottom dust duf tldr git-delta
+brew install fzf git wget ranger tmux neovim node neofetch stow lazygit ripgrep fd python@3.9 koekeishiya/formulae/yabai joshuto exa bat bottom dust duf tldr git-delta trash
 brew install --cask --no-quarantine google-chrome iterm2 qq sogouinput wechat visual-studio-code mos miniconda neovide spotify raycast ank fig
