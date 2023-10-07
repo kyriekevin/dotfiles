2fd3ce28d21c86ce1bb42173f461890cccdbff04
kyriekevin <2718629413@qq.com>
2023-10-08 00:05:44 +0800

    add some useful zsh config

--- zsh/.config/zsh/aliases.zsh
+++ zsh/.config/zsh/aliases.zsh
@@ -13,11 +13,16 @@ alias du=dust
 alias tl=tldr
 
 # other useful alias
+alias rmm="trash" # brew install trash
+
 alias pw="sudo poweroff"
 alias rb="sudo reboot"
 
 alias p=pwd
-alias c=clear
+# https://askubuntu.com/a/473770
+alias c="clear && printf '\e[3J'"
+alias cpwd="pwd | pbcopy && echo Current path has been copied to clipboard!"
+alias e=exit
 alias cat=bat
 alias df=duf
 alias ping=gping
@@ -34,7 +39,8 @@ alias ne=neovide
 
 alias top=btm
 
-# IP alias and functions
-alias ip="ifconfig -a | egrep -A 7 '^en0' | grep inet | grep -oE '((1?[0-9][0-9]?|2[0-4][0-9]|25[0-5])\.){3}(1?[0-9][0-9]?|2[0-4][0-9]|25[0-5])' | head -n 1"
-alias myip="curl -s http://checkip.dyndns.org/ | sed 's/[a-zA-Z<>/ :]//g'"
+alias dir="create_and_cd"
 
+# IP alias and functions
+alias ip="get_ip"
+alias ipl="get_ip_local"
--- zsh/.config/zsh/funcs.zsh
+++ zsh/.config/zsh/funcs.zsh
@@ -26,12 +26,50 @@ fnvim() {
   fi
 }
 
-dir() {
-    mkdir -p "$@" && cd "$@"
+# useage: create_and_cd [dir/path]
+function create_and_cd() {
+  local dir=$1
+  if [[ -d $dir ]]; then
+    cd $dir
+    return
+  fi
+  mkdir -p $dir && cd $dir
 }
 
-cpwd() {
-    pwd | pbcopy && echo "Current path has been copied to clipboard!"
+function rm() {
+  # https://iboysoft.com/questions/why-is-there-no-put-back-button-in-mac-trash.html
+  echo -e '\033[31mUse "rmm", or the full path i.e. "/bin/rm"\033[0m'
+  if ! command -v trash &> /dev/null; then
+    echo -e '\033[31mtrash command not found. Please install trash first.\033[0m'
+    return
+  fi
+  trash "$@"
 }
 
+function get_ip_local(){
+  ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1'
+}
 
+# usage: get_ip [count]
+function get_ip() {
+  local services=(
+    "https://api.ipify.org" # https://www.ipify.org/
+    "https://api64.ipify.org"
+    "https://ipinfo.io/ip" # https://ipinfo.io/
+    "https://ifconfig.me/ip" # https://ifconfig.me/
+  )
+
+  local count=${1:-${#services[@]}}
+
+  # split [0, count)
+  local services=(${services[@]:0:$count})
+
+  for service in ${services[@]}; do
+    local ip=$(curl -s $service)
+    if [[ -z $ip ]]; then
+      echo -e "${RED}$service${RESET}: ${RED}failed${RESET}"
+    else
+      echo -e "${GREEN}$service${RESET}: $ip"
+    fi
+  done
+}
