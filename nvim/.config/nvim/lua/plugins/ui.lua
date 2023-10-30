187731d880223cd3807ce1eed311f29dde350275
kyriekevin <2718629413@qq.com>
2023-10-30 13:19:25 +0800

    add neovim's notify plugin

--- nvim/.config/nvim/lua/plugins/ui.lua
+++ nvim/.config/nvim/lua/plugins/ui.lua
@@ -1,24 +1,42 @@
 return {
-	-- bufferline
-	{
-		"akinsho/bufferline.nvim",
-		event = "VeryLazy",
-		config = true,
-	},
+  -- bufferline
+  {
+    "akinsho/bufferline.nvim",
+    event = "VeryLazy",
+    config = true,
+  },
 
-	-- indent-blankline
-	{
-		"lukas-reineke/indent-blankline.nvim",
-		main = "ibl",
-		event = "VeryLazy",
-		config = true,
-	},
+  {
+    "shellRaining/hlchunk.nvim",
+    init = function()
+      vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI" }, { pattern = "*", command = "EnableHL", })
+      require('hlchunk').setup({
+        chunk = {
+          enable = true,
+          use_treesitter = true,
+          style = {
+            { fg = "#806d9c" },
+          },
+        },
+        indent = {
+          chars = { "│", "¦", "┆", "┊", },
+          use_treesitter = false,
+        },
+        blank = {
+          enable = false,
+        },
+        line_num = {
+          use_treesitter = true,
+        },
+      })
+    end
+  },
 
-	-- alpha-nvim
-	{
-		"goolord/alpha-nvim",
-		config = function()
-			require("alpha").setup(require("alpha.themes.dashboard").config)
-		end,
-	},
+  -- alpha-nvim
+  {
+    "goolord/alpha-nvim",
+    config = function()
+      require("alpha").setup(require("alpha.themes.dashboard").config)
+    end,
+  },
 }
