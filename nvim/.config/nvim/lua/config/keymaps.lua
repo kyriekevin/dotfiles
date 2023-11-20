5410b117b83750eee71b6f159334d3f9a66b3372
kyriekevin <2718629413@qq.com>
2023-11-20 13:27:19 +0800

    update neovim keymaps config

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -2,29 +2,52 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
-local map = vim.keymap.set
+local keymap = vim.keymap
+local opts = { noremap = true, silent = true }
+
+-- Increment/decrement
+keymap.set("n", "+", "<C-a>")
+keymap.set("n", "-", "<C-x>")
+
+-- Select all
+keymap.set("n", "<C-a>", "gg<S-v>G")
 
 -- Key mappings --
-map({ "n", "i", "v" }, "<Left>", "<Nop>")
-map({ "n", "i", "v" }, "<Right>", "<Nop>")
-map({ "n", "i", "v" }, "<Up>", "<Nop>")
-map({ "n", "i", "v" }, "<Down>", "<Nop>")
-map({ "n" }, ";", ":")
+keymap.set({ "n", "i", "v" }, "<Left>", "<Nop>")
+keymap.set({ "n", "i", "v" }, "<Right>", "<Nop>")
+keymap.set({ "n", "i", "v" }, "<Up>", "<Nop>")
+keymap.set({ "n", "i", "v" }, "<Down>", "<Nop>")
+keymap.set({ "n" }, ";", ":")
 
 -- better indenting
-map({ "v" }, "<", "<gv")
-map({ "v" }, ">", ">gv")
+keymap.set({ "v" }, "<", "<gv")
+keymap.set({ "v" }, ">", ">gv")
 
-map({ "i" }, "jk", "<Esc>")
-map({ "i" }, "kj", "<Esc>")
-map({ "i" }, "c-a", "<ESC>A")
+keymap.set({ "i" }, "jk", "<Esc>")
+keymap.set({ "i" }, "kj", "<Esc>")
+keymap.set({ "i" }, "<c-a>", "<ESC>A")
+
+-- New tab
+keymap.set("n", "te", ":tabedit<CR>")
+keymap.set("n", "<tab>", ":tabnext<Return>", opts)
+keymap.set("n", "<s-tab>", ":tabprev<Return>", opts)
+
+-- Split window
+keymap.set("n", "ss", ":split<Return>", opts)
+keymap.set("n", "sv", ":vsplit<Return>", opts)
+
+-- Move window
+keymap.set("n", "sh", "<C-w>h")
+keymap.set("n", "sk", "<C-w>k")
+keymap.set("n", "sj", "<C-w>j")
+keymap.set("n", "sl", "<C-w>l")
 
 -- markdown
-map({ "i" }, ",,", "<Esc>/<++><CR>:nohlsearch<CR>c4l")
-map({ "i" }, ",f", "<Esc>/<++><CR>:nohlsearch<CR>")
-map({ "i" }, ",n", "---<Enter><Enter>")
-map({ "i" }, ",c", "```<Enter><++><Enter>```<Enter><Enter><++><Esc>4kA")
-map({ "i" }, ",1", "#<Space><Enter><++><Esc>kA")
-map({ "i" }, ",2", "##<Space><Enter><++><Esc>kA")
-map({ "i" }, ",3", "###<Space><Enter><++><Esc>kA")
-map({ "i" }, ",4", "####<Space><Enter><++><Esc>kA")
+keymap.set({ "i" }, ",,", "<Esc>/<++><CR>:nohlsearch<CR>c4l")
+keymap.set({ "i" }, ",f", "<Esc>/<++><CR>:nohlsearch<CR>")
+keymap.set({ "i" }, ",n", "---<Enter><Enter>")
+keymap.set({ "i" }, ",c", "```<Enter><++><Enter>```<Enter><Enter><++><Esc>4kA")
+keymap.set({ "i" }, ",1", "#<Space><Enter><++><Esc>kA")
+keymap.set({ "i" }, ",2", "##<Space><Enter><++><Esc>kA")
+keymap.set({ "i" }, ",3", "###<Space><Enter><++><Esc>kA")
+keymap.set({ "i" }, ",4", "####<Space><Enter><++><Esc>kA")
