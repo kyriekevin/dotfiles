0b3c2f240c355e6d927a724e92ff6b642a877f00
kyriekevin <2718629413@qq.com>
2024-02-25 22:13:02 +0800

    update zsh vi-mode to support colemak layout

--- zsh/.config/zsh/cursor.zsh
+++ zsh/.config/zsh/cursor.zsh
@@ -1,3 +1,18 @@
+bindkey '^v' edit-command-line
+bindkey -v
+bindkey -M vicmd "k" vi-insert
+bindkey -M vicmd "K" vi-insert-bol
+bindkey -M vicmd "n" vi-backward-char
+bindkey -M vicmd "i" vi-forward-char
+bindkey -M vicmd "N" vi-beginning-of-line
+bindkey -M vicmd "I" vi-end-of-line
+bindkey -M vicmd "e" down-line-or-history
+bindkey -M vicmd "u" up-line-or-history
+bindkey -M vicmd "l" undo
+#bindkey -M vicmd "-" vi-rev-repeat-search
+bindkey -M vicmd "=" vi-repeat-search
+bindkey -M vicmd "h" vi-forward-word-end
+
 # Change cursor shape for different vi modes.
 function zle-keymap-select {
 	if [[ ${KEYMAP} == vicmd ]] || [[ $1 = 'block' ]]; then
