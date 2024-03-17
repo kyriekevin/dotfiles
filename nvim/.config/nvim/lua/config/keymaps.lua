47b267c6e14fd854d5b765972606ea6e07a5154f
kyriekevin <2718629413@qq.com>
2024-03-17 09:20:34 +0800

    update neovim config from scratch

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -1,61 +1,65 @@
--- Keymaps are automatically loaded on the VeryLazy event
--- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here
-
-local keymap = vim.keymap
+local keymap = vim.keymap -- for conciseness
 local opts = { noremap = true, silent = true }
 
--- Increment/decrement
-keymap.set("n", "+", "<C-a>")
-keymap.set("n", "-", "<C-x>")
+-- increment/decrement numbers
+keymap.set("n", "<leader>+", "<C-a>", { desc = "Increment number" }) -- increment
+keymap.set("n", "<leader>-", "<C-x>", { desc = "Decrement number" }) -- decrement
+
+-- set leader key to space
+keymap.set("n", "<Space>", "", opts)
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
 
 -- better navigation
 keymap.set({ "n", "v" }, "J", "5j")
 keymap.set({ "n", "v" }, "K", "5k")
-keymap.set({ "n", "v" }, "H", "0")
-keymap.set({ "n", "v" }, "L", "$")
+keymap.set({ "n", "v", "o", "x" }, "H", "^", opts)
+keymap.set({ "n", "v", "o", "x" }, "L", "g_", opts)
 keymap.set({ "n", "v" }, "W", "5w")
 keymap.set({ "n", "v" }, "B", "5b")
 
 -- Select all
 keymap.set("n", "<C-a>", "gg<S-v>G")
 
+-- clear search highlights
+keymap.set("n", "<leader>nh", ":nohl<CR>", { desc = "Clear search highlights" })
+
 -- Key mappings --
 keymap.set({ "n", "i", "v" }, "<Left>", "<Nop>")
 keymap.set({ "n", "i", "v" }, "<Right>", "<Nop>")
 keymap.set({ "n", "i", "v" }, "<Up>", "<Nop>")
 keymap.set({ "n", "i", "v" }, "<Down>", "<Nop>")
-keymap.set({ "n" }, ";", ":")
+keymap.set("n" , ";", ":")
 
 -- better indenting
-keymap.set({ "v" }, "<", "<gv")
-keymap.set({ "v" }, ">", ">gv")
+keymap.set("v" , "<", "<gv")
+keymap.set("v" , ">", ">gv")
 
-keymap.set({ "i" }, "jk", "<Esc>")
-keymap.set({ "i" }, "kj", "<Esc>")
-keymap.set({ "i" }, "<c-a>", "<ESC>A")
+-- use jk/kj to exit insert mode
+keymap.set("i", "jk", "<ESC>", { desc = "Exit insert mode with jk" })
+keymap.set("i", "kj", "<ESC>", { desc = "Exit insert mode with kj" })
+keymap.set("i" , "<c-a>", "<ESC>A")
 
--- New tab
-keymap.set("n", "te", ":tabedit<CR>")
-keymap.set("n", "<tab>", ":tabnext<Return>", opts)
-keymap.set("n", "<s-tab>", ":tabprev<Return>", opts)
+-- window management
+keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" }) -- split window vertically
+keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" }) -- split window horizontally
+keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" }) -- make split windows equal width & height
+keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" }) -- close current split window
 
--- Split window
-keymap.set("n", "ss", ":split<Return>", opts)
-keymap.set("n", "sv", ":vsplit<Return>", opts)
+keymap.set("n", "<leader>to", "<cmd>tabnew<CR>", { desc = "Open new tab" }) -- open new tab
+keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" }) -- close current tab
+keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" }) --  go to next tab
+keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" }) --  go to previous tab
+keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" }) --  move current buffer to new tab
 
--- Move window
-keymap.set("n", "sh", "<C-w>h")
-keymap.set("n", "sk", "<C-w>k")
-keymap.set("n", "sj", "<C-w>j")
-keymap.set("n", "sl", "<C-w>l")
+keymap.set("x", "p", [["_dP]])
 
 -- markdown
-keymap.set({ "i" }, ",,", "<Esc>/<++><CR>:nohlsearch<CR>c4l")
-keymap.set({ "i" }, ",f", "<Esc>/<++><CR>:nohlsearch<CR>")
-keymap.set({ "i" }, ",n", "---<Enter><Enter>")
-keymap.set({ "i" }, ",c", "```<Enter><++><Enter>```<Enter><Enter><++><Esc>4kA")
-keymap.set({ "i" }, ",1", "#<Space><Enter><++><Esc>kA")
-keymap.set({ "i" }, ",2", "##<Space><Enter><++><Esc>kA")
-keymap.set({ "i" }, ",3", "###<Space><Enter><++><Esc>kA")
-keymap.set({ "i" }, ",4", "####<Space><Enter><++><Esc>kA")
+keymap.set("i" , ",,", "<Esc>/<++><CR>:nohlsearch<CR>c4l")
+keymap.set("i" , ",f", "<Esc>/<++><CR>:nohlsearch<CR>")
+keymap.set("i" , ",n", "---<Enter><Enter>")
+keymap.set("i" , ",c", "```<Enter><++><Enter>```<Enter><Enter><++><Esc>4kA")
+keymap.set("i" , ",1", "#<Space><Enter><++><Esc>kA")
+keymap.set("i" , ",2", "##<Space><Enter><++><Esc>kA")
+keymap.set("i" , ",3", "###<Space><Enter><++><Esc>kA")
+keymap.set("i" , ",4", "####<Space><Enter><++><Esc>kA")
--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -1,6 +1,63 @@
--- Options are automatically loaded before lazy.nvim startup
--- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
--- Add any additional options here
+-- Enable LazyVim auto format
+vim.g.autoformat = true
 
-vim.opt.mouse = ""
-vim.opt.title = true
+vim.g.netrw_banner = 0
+
+local opt = vim.opt -- for conciseness
+
+-- line numbers
+opt.relativenumber = true -- show relative line numbers
+opt.number = true -- shows absolute line number on cursor line (when relative number is on)
+
+-- tabs & indentation
+opt.tabstop = 2 -- 2 spaces for tabs (prettier default)
+opt.shiftwidth = 2 -- 2 spaces for indent width
+opt.expandtab = true -- expand tab to spaces
+opt.autoindent = true -- copy indent from current line when starting new one
+opt.smartindent = true -- Insert indents automatically
+
+-- line wrapping
+opt.wrap = false -- disable line wrapping
+
+-- search settings
+opt.ignorecase = true -- ignore case when searching
+opt.smartcase = true -- if you include mixed case in your search, assumes you want case-sensitive
+
+-- cursor line
+opt.cursorline = true -- highlight the current cursor line
+
+-- appearance
+opt.termguicolors = true
+opt.signcolumn = "yes" -- show sign column so that text doesn't shift
+
+-- backspace
+opt.backspace = "indent,eol,start" -- allow backspace on indent, end of line or insert mode start position
+
+-- clipboard
+opt.clipboard:append("unnamedplus") -- use system clipboard as default register
+
+-- split windows
+opt.splitright = true -- split vertical window to the right
+opt.splitbelow = true -- split horizontal window to the bottom
+
+-- turn off swapfile
+opt.swapfile = false
+
+opt.showmode = false -- we don't need to see things like -- INSERT -- anymore
+opt.mouse = ""
+opt.autowrite = true -- Enable auto write
+opt.completeopt = "menu,menuone,noselect"
+opt.confirm = true -- Confirm to save changes before exiting modified buffer
+opt.laststatus = 3 -- global statusline
+opt.list = true -- Show some invisible characters (tabs...
+opt.pumblend = 10 -- Popup blend
+opt.pumheight = 10 -- Maximum number of entries in a popup
+opt.spelllang = { "en" }
+opt.undofile = true
+opt.undolevels = 10000
+opt.virtualedit = "block" -- Allow cursor to move where there is no text in visual block mode
+opt.wildmode = "longest:full,full" -- Command-line completion mode
+
+if not vim.g.vscode then
+  opt.timeoutlen = 300 -- Lower than default (1000) to quickly trigger which-key
+end
