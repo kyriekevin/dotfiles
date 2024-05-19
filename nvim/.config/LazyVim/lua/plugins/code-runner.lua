12134bba910c8ffbff1ca37c6bb7d4ba8be231ae
kyriekevin <2718629413@qq.com>
2024-05-19 21:58:00 +0800

    Modify the neovim plugin configuration to resolve key conflicts

--- nvim/.config/LazyVim/lua/plugins/code-runner.lua
+++ nvim/.config/LazyVim/lua/plugins/code-runner.lua
@@ -106,10 +106,10 @@ return {
           visual_send = "<space>rc",
           send_file = "<space>rf",
           send_line = "<space>rl",
-          send_mark = "<space>rm",
-          mark_motion = "<space>rmc",
-          mark_visual = "<space>rmc",
-          remove_mark = "<space>rmd",
+          -- send_mark = "<space>rm",
+          -- mark_motion = "<space>rmc",
+          -- mark_visual = "<space>rmc",
+          -- remove_mark = "<space>rmd",
           cr = "<space>r<cr>",
           interrupt = "<space>r<space>",
           exit = "<space>rq",
@@ -125,7 +125,7 @@ return {
 
       -- iron also has a list of commands, see :h iron-commands for all available commands
       vim.keymap.set("n", "<space>rs", "<cmd>IronRepl<cr>")
-      vim.keymap.set("n", "<space>rr", "<cmd>IronRestart<cr>")
+      -- vim.keymap.set("n", "<space>rr", "<cmd>IronRestart<cr>")
       vim.keymap.set("n", "<space>rF", "<cmd>IronFocus<cr>")
       vim.keymap.set("n", "<space>rh", "<cmd>IronHide<cr>")
     end,
