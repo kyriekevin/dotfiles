dcc8f85c7c8761cd64765f6dc5ccde0e0eba9ef1
kyriekevin <2718629413@qq.com>
2024-07-28 22:09:05 +0800

    Move the original nvim configuration to nvim.old, and configure vscode under nvim

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,1 +1,43 @@
-require("config")
+vim.g.mapleader = " "
+
+vim.cmd("nmap <leader>rc :e ~/.config/nvim/init.lua<cr>")
+
+vim.keymap.set({ "n", "v" }, "W", "5w")
+vim.keymap.set({ "n", "v" }, "B", "5b")
+
+vim.keymap.set("v", "p", "P")
+vim.keymap.set("n", "U", "<C-r>")
+vim.keymap.set("n", "<Esc>", ":nohlsearch<cr>")
+
+vim.cmd("nmap j gj")
+vim.cmd("nmap k gk")
+
+vim.opt.clipboard = "unnamedplus"
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable",
+		lazypath,
+	})
+end
+vim.opt.rtp:prepend(vim.env.LAZY or lazypath)
+
+require("lazy").setup({
+	spec = {
+		{ "LazyVim/LazyVim", import = "lazyvim.plugins" },
+		{ import = "lazyvim.plugins.extras.vscode" },
+	},
+	{
+		"vscode-neovim/vscode-multi-cursor.nvim",
+		event = "VeryLazy",
+		cond = not not vim.g.vscode,
+		opts = {},
+	},
+})
