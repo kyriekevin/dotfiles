3b0367b99f6070e2f3a43411958770e46342f7f1
Kyrie <zyzkyrie@outlook.com>
2024-12-09 00:45:24 +0800

    feat(avante):  let neovim interact with ai like a cursor (#6)
    * feat(copilot): add neovim copilot plugin config
    
    * feat(avante): add avante plugin to let neovim use ai like cursor
    
    * feat(avante): update avante plugin config

---
+++ nvim/.config/nvim/lua/plugins/ai.lua
@@ -0,0 +1,78 @@
+return {
+	{
+		"zbirenbaum/copilot.lua",
+		cmd = "Copilot",
+		build = ":Copilot auth",
+		event = "InsertEnter",
+		config = function()
+			require("copilot").setup({
+				suggestion = {
+					enabled = not vim.g.ai_cmp,
+					auto_trigger = true,
+					keymap = {
+						accept = false, -- handled by nvim-cmp / blink.cmp
+						next = "<M-]>",
+						prev = "<M-[>",
+					},
+				},
+				panel = { enabled = false },
+				filetypes = {
+					markdown = true,
+					help = true,
+				},
+			})
+		end,
+	},
+	{
+		"yetone/avante.nvim",
+		event = "VeryLazy",
+		lazy = false,
+		version = false, -- set this if you want to always pull the latest change
+		opts = {
+			-- add any opts here
+			provider = "azure",
+			azure = {
+				endpoint = vim.fn.getenv("AZURE_ENDPOINT") or "",
+				deployment = vim.fn.getenv("AZURE_DEPLOYMENT") or "",
+				api_version = vim.fn.getenv("AZURE_API_VERSION") or "",
+			},
+		},
+		-- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
+		build = "make",
+		-- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
+		dependencies = {
+			"stevearc/dressing.nvim",
+			"nvim-lua/plenary.nvim",
+			"MunifTanjim/nui.nvim",
+			--- The below dependencies are optional,
+			"hrsh7th/nvim-cmp", -- autocompletion for avante commands and mentions
+			"nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
+			"zbirenbaum/copilot.lua", -- for providers='copilot'
+			{
+				-- support for image pasting
+				"HakonHarnes/img-clip.nvim",
+				event = "VeryLazy",
+				opts = {
+					-- recommended settings
+					default = {
+						embed_image_as_base64 = false,
+						prompt_for_file_name = false,
+						drag_and_drop = {
+							insert_mode = true,
+						},
+						-- required for Windows users
+						use_absolute_path = true,
+					},
+				},
+			},
+			{
+				-- Make sure to set this up properly if you have lazy=true
+				"MeanderingProgrammer/render-markdown.nvim",
+				opts = {
+					file_types = { "markdown", "Avante" },
+				},
+				ft = { "markdown", "Avante" },
+			},
+		},
+	},
+}
