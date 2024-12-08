5895334b54c97477d79af16bf049ed75193f487d
zyz <zyzkyrie@outlook.com>
2024-12-08 16:06:58 +0800

    chore(scripts): update script to install neovim and tools

--- scripts/nvim.sh
+++ scripts/nvim.sh
@@ -5,3 +5,13 @@ source "${SCRIPT_DIR}/lib/common.sh"
 
 step "Installing nvim if not already installed"
 install nvim
+
+step "Installing fzf if not already installed"
+install fzf
+
+step "Installing ripgrep if not already installed"
+install ripgrep
+
+step "Installing macism if not already installed"
+brew tap laishulu/homebrew
+install macism
