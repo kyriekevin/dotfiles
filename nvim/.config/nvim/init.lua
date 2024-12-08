330ad7ec5e3e9aa4dc132d6f592cec1a7a2826d3
zyz <zyzkyrie@outlook.com>
2024-12-08 16:03:45 +0800

    feat(im-select): add im-select plugin to auto switch input method

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -27,24 +27,21 @@ Guide:
 -- Set <space> as the leader key
 -- See `:help mapleader`
 -- NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
-
--- Set to true if you have a Nerd Font installed and selected in the terminal
-vim.g.have_nerd_font = false
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
 
 -- [[ Setting options ]]
-require "config.options"
+require("config.options")
 
 -- [[ Basic Keymaps ]]
-require "config.keymaps"
+require("config.keymaps")
 
 -- [[ Basic AutoCmds ]]
-require "config.autocmds"
+require("config.autocmds")
 
 -- [[ Install `lazy.nvim` plugin manager ]]
 -- [[ Configure and install plugins ]]
-require "config.lazy"
+require("config.lazy")
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
