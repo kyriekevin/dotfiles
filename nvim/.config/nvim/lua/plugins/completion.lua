8d5ae42ee3308954fe5bfb654805f550177b7f99
zyz <zyzkyrie@outlook.com>
2025-05-05 21:28:17 +0800

    feat(blink cmp): update blink cmp configuration to adjust the display when completing

--- nvim/.config/nvim/lua/plugins/completion.lua
+++ nvim/.config/nvim/lua/plugins/completion.lua
@@ -45,9 +45,19 @@ return {
 			},
 
 			completion = {
-				documentation = { auto_show = true, auto_show_delay_ms = 500 },
+				documentation = {
+					auto_show = true,
+					auto_show_delay_ms = 200,
+					window = {
+						border = "rounded",
+					},
+				},
 				keyword = { range = "full" },
 				list = { selection = { preselect = false, auto_insert = true } },
+				menu = {
+					border = "rounded",
+					winhighlight = "Normal:BlinkCmpDoc,FloatBorder:BlinkCmpDocBorder,EndOfBuffer:BlinkCmpDoc",
+				},
 			},
 
 			-- Default list of enabled providers defined so that you can extend it
