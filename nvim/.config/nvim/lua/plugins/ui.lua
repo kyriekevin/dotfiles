01bba1e3c55fbb444685a42bf97b8e2e29678b77
zyz <zyzkyrie@outlook.com>
2025-05-05 22:56:52 +0800

    feat(ui): optimize neovim ui configuration, including indentation and bracket colors

--- nvim/.config/nvim/lua/plugins/ui.lua
+++ nvim/.config/nvim/lua/plugins/ui.lua
@@ -170,15 +170,49 @@ return {
 		end,
 	},
 
-	-- @plugin mini.indentscope
-	-- @category ui
+	-- @plugin rainbow-delimiters
+	-- @category ui.syntax-highlighting
+	-- @description Color-codes nested parentheses, brackets, and braces for easier visual parsing
+	{
+		"hiphish/rainbow-delimiters.nvim",
+		event = "VeryLazy",
+		opts = {},
+	},
+
+	-- @plugin indent blankline
+	-- @category ui.indentation
 	-- @description Visualize and work with indentation scope
 	{
-		"echasnovski/mini.indentscope",
-		event = "VeryLazy",
-		opts = {
-			draw = { delay = 50 },
-		},
+		"lukas-reineke/indent-blankline.nvim",
+		main = "ibl",
+		config = function()
+			local highlight = {
+				"RainbowRed",
+				"RainbowYellow",
+				"RainbowBlue",
+				"RainbowOrange",
+				"RainbowGreen",
+				"RainbowViolet",
+				"RainbowCyan",
+			}
+			local hooks = require("ibl.hooks")
+			-- create the highlight groups in the highlight setup hook, so they are reset
+			-- every time the colorscheme changes
+			hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
+				vim.api.nvim_set_hl(0, "RainbowRed", { fg = "#E06C75" })
+				vim.api.nvim_set_hl(0, "RainbowYellow", { fg = "#E5C07B" })
+				vim.api.nvim_set_hl(0, "RainbowBlue", { fg = "#61AFEF" })
+				vim.api.nvim_set_hl(0, "RainbowOrange", { fg = "#D19A66" })
+				vim.api.nvim_set_hl(0, "RainbowGreen", { fg = "#98C379" })
+				vim.api.nvim_set_hl(0, "RainbowViolet", { fg = "#C678DD" })
+				vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#56B6C2" })
+			end)
+
+			vim.g.rainbow_delimiters = { highlight = highlight }
+			require("ibl").setup({ scope = { highlight = highlight } })
+
+			hooks.register(hooks.type.SCOPE_HIGHLIGHT, hooks.builtin.scope_highlight_from_extmark)
+		end,
 	},
 
 	-- @plugin mini.icons
